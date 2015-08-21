1c503233eec7f8d0375c24ae01f4758de209d4e8
cramirezp <cristobal.ramirez@alumnos.usm.cl>
2015-08-21 12:41:38 -0300

    rpi_pgio pin handler

---
+++ headers/rpi_gpio.h
@@ -0,0 +1,27 @@
+#include <map>
+#include <vector>
+
+#include <stdint.h>
+#include <bcm2835.h>
+
+class Rpi_GPIO{
+private:
+	// Pin access
+	std::map<int, uint8_t> pin;
+	// Pin state <- TODO struct with, none/IN/OUT/SPI/CLK/etc.
+	std::map<uint8_t, bool> pin_is_used;
+
+protected:
+public:
+	
+	Rpi_GPIO();
+	~Rpi_GPIO();
+
+	bool init();
+	bool close();
+
+	bool pin_exists(uint8_t);
+
+	bool add_sensor(uint8_t pin_number);
+	bool add_sensor<N>(std::array<uint8_t, N> pin_number);
+};
