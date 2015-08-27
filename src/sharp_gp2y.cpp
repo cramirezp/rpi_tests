205351c6072cb5fa0a609c6163562e8eeef280ac
cramirezp <cristobal.ramirez@alumnos.usm.cl>
2015-08-27 19:33:29 -0300

    removed cmment

--- src/sharp_gp2y.cpp
+++ src/sharp_gp2y.cpp
@@ -2,23 +2,20 @@
 
 #include "rpi_gpio.h"
 #include "rpi_gpio_sensor.h"
-
+	
 class sharp_gp2y: public Rpi_GPIO_sensor{
 private:
-	float meters;
+	uint8_t pin;
 protected:
 public:
-	sharp_gp2y():meters(.0){}
+	sharp_gp2y():pin(0){}
 	~sharp_gp2y(){}
 
 	virtual void tick(){
-		//Rpi_GPIO_sensor::tick();
-		uint8_t pin11 = bcm2835_gpio_lev(RPI_GPIO_P1_11);
-
-		meters = pin11*1.3 + 0.2;
+		pin = bcm2835_gpio_lev(RPI_GPIO_P1_11);
 	}
 
-	float measurement(){ return meters;}
+	float measurement(){ return ((float)pin)*1.3 + 0.2; }
 };
 
 int main(int argc, char *argv[]){
